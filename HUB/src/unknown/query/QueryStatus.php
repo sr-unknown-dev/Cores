eff1712d6caa7443ce9f66d375035b764ef94bbc
Sr.Unkn0wn <idk811959@gmail.com>
2025-05-15 22:01:27 -0600

    fix: QueryStatus.php

--- HUB/src/unknown/query/QueryStatus.php
+++ HUB/src/unknown/query/QueryStatus.php
@@ -10,28 +10,36 @@ class QueryStatus {
     ) {}
 
     public function query(): ?array {
-        $socket = @fsockopen($this->ip, $this->port, $errno, $errstr, 1.5);
-
+        $socket = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
         if (!$socket) {
-            return null; // Servidor offline
-        }
-
-        fwrite($socket, "\xFE");
-        $data = fread($socket, 512);
-        fclose($socket);
-
-        if ($data === false || substr($data, 0, 1) !== "\xFF") {
             return null;
         }
 
-        $data = substr($data, 3);
-        $data = iconv("UTF-16BE", "UTF-8", $data);
-        $data = explode("§", $data);
+        socket_set_option($socket, SOL_SOCKET, SO_RCVTIMEO, ["sec"=>1,"usec"=>500000]);
 
+        $pk = "\x01\x00\x00\x00\x00\x00\x00\x00";
+        socket_sendto($socket, $pk, strlen($pk), 0, $this->ip, $this->port);
+
+        $buffer = '';
+        $from = '';
+        $port = 0;
+
+        if (@socket_recvfrom($socket, $buffer, 2048, 0, $from, $port) === false) {
+            socket_close($socket);
+            return null;
+        }
+
+        socket_close($socket);
+
+        if (strlen($buffer) < 35) {
+            return null;
+        }
+
+        $data = explode(';', substr($buffer, 35));
         return [
-            "motd" => $data[0] ?? "Unknown",
-            "online" => (int)($data[1] ?? 0),
-            "max" => (int)($data[2] ?? 0)
+            "motd" => $data[1] ?? "Unknown",
+            "online" => (int)($data[4] ?? 0),
+            "max" => (int)($data[5] ?? 0),
         ];
     }
 }
