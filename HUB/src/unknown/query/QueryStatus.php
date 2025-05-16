b3608915b26582fc49a371599f93a99195fdc862
Sr.Unkn0wn <idk811959@gmail.com>
2025-05-15 22:12:38 -0600

    update: IP SERVER

--- HUB/src/unknown/query/QueryStatus.php
+++ HUB/src/unknown/query/QueryStatus.php
@@ -15,27 +15,28 @@ class QueryStatus {
             return null;
         }
 
-        socket_set_option($socket, SOL_SOCKET, SO_RCVTIMEO, ["sec"=>1,"usec"=>500000]);
+        // Timeout de recepción (1.5 segundos)
+        socket_set_option($socket, SOL_SOCKET, SO_RCVTIMEO, ["sec" => 1, "usec" => 500000]);
 
-        $pk = "\x01\x00\x00\x00\x00\x00\x00\x00";
+        // Paquete Ping estándar Bedrock (1 byte 0x01 + 8 bytes 0x00)
+        $pk = "\x01" . str_repeat("\x00", 8);
+
         socket_sendto($socket, $pk, strlen($pk), 0, $this->ip, $this->port);
 
         $buffer = '';
         $from = '';
         $port = 0;
 
-        if (@socket_recvfrom($socket, $buffer, 2048, 0, $from, $port) === false) {
-            socket_close($socket);
-            return null;
-        }
-
+        $recv = @socket_recvfrom($socket, $buffer, 2048, 0, $from, $port);
         socket_close($socket);
 
-        if (strlen($buffer) < 35) {
+        if ($recv === false || strlen($buffer) < 35) {
             return null;
         }
 
+        // El payload útil empieza en el byte 35 (después de encabezados RakNet)
         $data = explode(';', substr($buffer, 35));
+
         return [
             "motd" => $data[1] ?? "Unknown",
             "online" => (int)($data[4] ?? 0),
